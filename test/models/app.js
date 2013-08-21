9c79b1e80f40f94661b21ef7fed6117c0baf7e3b
Paul Bowsher <pbowsher@globalpersonals.co.uk>
2013-08-21 10:50:53 +0100

    Switch to (err, res) pattern

--- test/models/app.js
+++ test/models/app.js
@@ -21,23 +21,35 @@ describe("App", function() {
 
     describe("for an existing app", function() {
       it("yields the app instance", function(done) {
-        App.find("boofar", function(app) {
+        App.find("boofar", function(err, app) {
           app.should.be.an.instanceof(App);
           done();
         });
       });
       it("populates the name", function(done) {
-        App.find("boofar", function(app) {
+        App.find("boofar", function(err, app) {
           app.name.should.equal("boofar");
+          done();
+        });
+      });
+      it("yields a null error", function(done) {
+        App.find("boofar", function(err, app) {
+          should.not.exist(err);
           done();
         });
       });
     });
 
     describe("for a non-existant app", function() {
-      it("yields NotFound", function(done) {
-        App.find("foobar", function(app) {
-          app.constructor.name.should.eql("NotFound");
+      it("yields a NotFound error", function(done) {
+        App.find("foobar", function(err, app) {
+          err.should.equal("Not Found")
+          done();
+        });
+      });
+      it("yields no app", function(done) {
+        App.find("foobar", function(err, app) {
+          should.not.exist(app);
           done();
         });
       });
