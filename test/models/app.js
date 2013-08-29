7a1a925192b8cfda32205842169f13decec24cd7
Paul Bowsher <pbowsher@globalpersonals.co.uk>
2013-08-29 10:02:34 +0100

    Remove App.find() as it is redundant

--- test/models/app.js
+++ test/models/app.js
@@ -16,48 +16,6 @@ afterEach(function(done) {
 });
 
 describe("App", function() {
-  describe(".find", function() {
-    beforeEach(function(done){
-      redis.sadd("griddle:apps", "boofar", done);
-    });
-
-    describe("for an existing app", function() {
-      it("yields the app instance", function(done) {
-        App.find("boofar", function(err, app) {
-          app.should.be.an.instanceof(App);
-          done();
-        });
-      });
-      it("populates the name", function(done) {
-        App.find("boofar", function(err, app) {
-          app.name.should.equal("boofar");
-          done();
-        });
-      });
-      it("yields a null error", function(done) {
-        App.find("boofar", function(err) {
-          should.not.exist(err);
-          done();
-        });
-      });
-    });
-
-    describe("for a non-existant app", function() {
-      it("yields a NotFound error", function(done) {
-        App.find("foobar", function(err) {
-          err.should.equal("Not Found");
-          done();
-        });
-      });
-      it("yields no app", function(done) {
-        App.find("foobar", function(err, app) {
-          should.not.exist(app);
-          done();
-        });
-      });
-    });
-  });
-
   describe(".all", function() {
     beforeEach(function(done) {
       redis.sadd("griddle:apps", "foobar", "bazqux", done);
