f70d6a7b477828a090503c6767f5f8c6872fb631
Paul Bowsher <pbowsher@globalpersonals.co.uk>
2013-08-30 13:37:20 +0100

    Foobar not weeee

--- test/models/app.js
+++ test/models/app.js
@@ -51,10 +51,10 @@ describe("App", function() {
 
   describe("#save()", function() {
     it("adds the app name to the set", function(done) {
-      var app = new App("weeee");
+      var app = new App("foobar");
       app.save(function(err) {
         should.not.exist(err);
-        redis.sismember("griddle:apps", "weeee", function(err, result) {
+        redis.sismember("griddle:apps", "foobar", function(err, result) {
           result.should.equal(1);
           done();
         });
@@ -66,13 +66,13 @@ describe("App", function() {
   describe("property", function() {
     describe("#name", function() {
       it("contains the name the App was instantiated with", function() {
-        var app = new App("weeee");
-        app.name.should.equal("weeee");
+        var app = new App("foobar");
+        app.name.should.equal("foobar");
       });
 
       it("is readonly", function() {
-        var app = new App("weeee");
-        (function() {app.name = "woooo";}).should.throw(/read ?only/);
+        var app = new App("foobar");
+        (function() {app.name = "barfoo";}).should.throw(/read ?only/);
       });
     });
   });
